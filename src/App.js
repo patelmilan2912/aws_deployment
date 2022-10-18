58e2f896be793c7a5244f32a33371e093291d6d9
Milan Patel <patelmilan2912@gmail.com>
2022-10-18 12:22:37 -0400

    Challenge Commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in less than a day.
+          This is my first deployment to the AWS.
         </a>
       </header>
     </div>
